23386cb6cf62af7630ec1f3dd9e930f02002ee6b
kabra4 <kabra0413@gmail.com>
2021-04-03 10:51:31 +0500

    bug fix

--- public/scripts/taskList.js
+++ public/scripts/taskList.js
@@ -8,6 +8,7 @@ function colorTaskList() {
     for (let i = 0; i < allTask.length; i++){
         let countDownDate = new Date(allTask[i].id).getTime();
         let days = Math.round((countDownDate - now) / (1000 * 60 * 60 * 24));
+        let hours = Math.round((countDownDate - now) / (1000 * 60 * 60));
         let borColor, deadline, bgColor;
         if (days < 0){
             borColor = '#f00'
@@ -16,7 +17,8 @@ function colorTaskList() {
         } else if (days < 7){
             borColor = '#ffaa00'
             bgColor = '#ffe8c6'
-            deadline = ' ' + days.toString() + ' days'
+            if (days > 1) deadline = ' ' + days.toString() + ' days'
+            else deadline = ' ' + hours.toString() + ' hours'
         } else {
             borColor = '#00ff00'
             bgColor = '#d8ffd8'
